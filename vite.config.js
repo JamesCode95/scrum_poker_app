1e1c0a97457581fd3e7d6c2898970fef2fc9979f
JamesCode95 <bradleyjames353@gmail.com>
2025-04-02 22:32:21 +0100

    Clean up

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',  // Set root to the current directory (adjust if needed)
+  root: '.',  
   build: {
+    outDir: 'build', 
     rollupOptions: {
-      input: './index.html', // Ensure Vite knows about the entry point
+      input: './index.html', 
     },
   },
 });
