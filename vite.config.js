099fd51909c69ed2372d925ddbcccc62e08164bb
JamesCode95 <bradleyjames353@gmail.com>
2025-04-06 21:38:31 +0100

    Current latest

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/scrum_poker_app/',
   plugins: [react()],
   build: {
     outDir: 'dist',
